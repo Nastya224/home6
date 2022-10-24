4a1751de933386f2220e000bf5022db6d594f549
Nastya_keks <aagolunova@edu.hse.ru>
2022-10-24 12:01:14 +0300

    second

--- Program.cs
+++ Program.cs
@@ -1,7 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-ggggggg
-ggg
+﻿Console.Write("Кол-во чисел:");
+int N = int.Parse(Console.ReadLine());
+
+int counter = 0;
+
+for (int k =0; k < N; k++)
+{
+    int number = int.Parse(Console.ReadLine()!);
+    if (number>0)
+    {
+        counter++;
+    }
+}
+Console.Write("Кол-во чисел больше нуля:");
+Console.WriteLine(counter);
